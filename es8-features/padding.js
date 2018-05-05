8e12e456ace3880a9f806e6db9261820905734cb
Carlos Augusto Grispan <cagrispan@gmail.com>
2018-05-05 10:52:46 -0300

    Padding

---
+++ es8-features/padding.js
@@ -0,0 +1,8 @@
+console.log('AA'.padStart(2, '--')); // AA
+console.log('AA'.padStart(4, '--')); // --AA
+console.log('AA'.padStart(6, '--')); // ----AA
+
+console.log('AA'.padEnd(2, '--')); // AA
+console.log('AA'.padEnd(4, '--')); // AA--
+console.log('AA'.padEnd(6, '--')); // AA----
+
