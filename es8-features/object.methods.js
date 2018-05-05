6db3a294d2dea632fad03756520c0bfc8bc27f42
Carlos Augusto Grispan <cagrispan@gmail.com>
2018-05-05 11:11:19 -0300

    Object Methods

---
+++ es8-features/object.methods.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'Carlos',
+    age: 30,
+    nationality: 'Brazilian'
+};
+
+console.log(Object.entries(person)); // [ [ 'name', 'Carlos' ], [ 'age', 30 ], [ 'nationality', 'Brazilian' ] ]
+console.log(Object.values(person)); // [ 'Carlos', 30, 'Brazilian' ]
